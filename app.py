19c0244661c39bae543c7e7a39a4eb0f088c8c43
h6h60314-a11y <h6h60314@gmail.com>
2025-12-23 14:36:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # App config
 # ==================================================
 st.set_page_config(
-    page_title="進貨課效能平台",
+    page_title="大豐物流-作業平台",
     page_icon="🏭",
     layout="wide",
 )
@@ -27,4 +27,5 @@ pages = {
 }
 
 st.navigation(pages).run()
+
 
